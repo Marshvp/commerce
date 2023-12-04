63f1baa247bf7b6e1d52177063af8e052d9aeac3
Marshall <marshallpowell13@gmail.com>
2023-12-04 21:19:14 +0000

    CS50W commerce init

---
+++ auctions/models.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    pass
+
+
+'''
+class listings()
+
+
+
+
+
+
+class bids()
+
+
+
+
+class comments()
+'''
