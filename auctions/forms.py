8057c4197dbb40bb65df85e62c4b2648a7a8c731
Marshall <marshallpowell13@gmail.com>
2023-12-05 22:18:23 +0000

    New-listing model made and form added

---
+++ auctions/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from .models import Listing
+
+class ListingForm(forms.ModelForm):
+    class Meta:
+        model = Listing
+        fields = ['listing_title', 'listing_description', 'listing_starting_price']
--- auctions/models.py
+++ auctions/models.py
@@ -6,11 +6,27 @@ class User(AbstractUser):
     pass
 
 
+class Listing(models.Model):
+                listing_title = models.CharField(max_length=255)
+                listing_description = models.TextField()
+                listing_current_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True, default=None)
+                listing_starting_price = models.DecimalField(max_digits=10, decimal_places=2)
+                seller = models.ForeignKey(User, on_delete=models.CASCADE)
+
+                created_at = models.DateTimeField(auto_now_add=True)
+                updated_at = models.DateTimeField(auto_now=True)
+                
+                # Status
+                STATUS_CHOICES = [
+                    ('active', 'Active'),
+                    ('closed', 'Closed'),
+                    ('pending', 'Pending'),
+                ]
+                status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='active')
+
+                def __str__(self):
+                    return self.listing_title
 '''
-class listings()
-
-
-
 
 
 
